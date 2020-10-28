fd2ffc9214179789700c699faf3f1434d017cfcc
Simon Kaminski <szy.kaminski@gmail.com>
2020-10-28 15:29:41 +0100

    Create and populate code-first model for Product and Category

---
+++ EFCoreWPF-MD/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreWPF_MD
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
